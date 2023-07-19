76821323ec7addc0ee889ddc68f6061870a26f99
User <maxkulezenin@gmail.com>
2023-07-19 09:01:19 +0300

    Импортировали первые функции

--- Text Transformation/freq_of_symbol.py
+++
@@ -1,6 +0,0 @@
-def freq_of_symbol(text: str, symbols_for_freq: set[str]) -> float:
-    symbols_count = 0
-    for symbol in symbols_for_freq:
-        symbols_count += text.count(symbol)
-
-    return symbols_count / len(text)
