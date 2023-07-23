65f3124d30df4b4c63cdd6ed32aa07233b45cb9d
peepeg <ghost66141@gmail.com>
2023-07-23 22:23:52 +0700

    Just in case

--- ML/features/features_words.py
+++ ML/features/features_words.py
@@ -1,9 +1,14 @@
 from collections import Counter
+from typing import Callable
+
+from natasha import Doc
+from functools import wraps
 
 
 def frequency_of_a(freq_words: Counter) -> float:
     keys = freq_words['а']
     return keys
+
 
 
 def frequency_of_longest_word(freq_words: Counter) -> float:
@@ -19,6 +24,13 @@ def count_upper_words(freq_words: Counter) -> float:
         if key.isupper():
             up_count += freq_words[key]
             return up_count
+
+
+special_words = 'в на с за к по из у от для во без до о через со при про' \
+                    ' об ко над из-за из-под под и что но а да хотя когда чтобы ' \
+                    'если тоже или зато будто не как же ж даже бы ли только вот то' \
+                    'ни лишь ведь вон нибудь уже либо'.split()
+
 
 func_words = [count_upper_words, frequency_of_longest_word, frequency_of_a]
 def get_feature_word(doc):
